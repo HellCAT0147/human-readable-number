f8a8c9561c7dc477427c55a12a95b655d031815d
HellCAT0147 <letscheckmail23@gmail.com>
2023-01-24 20:27:46 +0500

    feat: remove variable

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ module.exports = function toReadable (number) {
   const small = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
   'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
   const middle = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-  const big = 'hundred';
   let HRN = ''; // Human Readable Number
   if (number / 100 >= 1)
-    HRN += small[Math.floor(number / 100)] + ' ' + big + ' ';
+    HRN += small[Math.floor(number / 100)] + ' hundred ';
   if (number % 100 > 19)
     HRN += middle[Math.floor(number % 100 / 10)] + ' ';
   if ((number % 10 > 0 || number % 100 == 10) && number % 100 < 20) // 
